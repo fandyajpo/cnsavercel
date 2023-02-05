dfab8a199cf39a131d4e3554e255b322c603a058
fandyajpo <fandyglitch3@gmail.com>
2023-02-05 18:14:51 +0800

    csava

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+require("dotenv").config();
+const cors = require("@koa/cors");
+const { koaBody } = require("koa-body");
+
+const Koa = require("koa");
+const Router = require("koa-router");
+
+const app = new Koa();
+
+app.use(cors());
+app.use(koaBody());
+
+const Public = new Router();
+app.use(Public.routes());
+app.use(Public.allowedMethods());
+
+app.on("error", (err, ctx) => {
+  console.log({ hasError: "I guess yes", status: err, route: ctx });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App Lister to ${process.env.PORT}`);
+});
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -14,3 +37,16 @@ client.on("interactionCreate", async (interaction) => {
 });
 
 client.login(process.env.TOKEN);
+const Fan = async (ctx) => {
+  try {
+    ctx.response.status = 200;
+    return (ctx.body = {
+      message: "Alive",
+    });
+  } catch (err) {
+    ctx.response.status = 400;
+    return (ctx.body = { status: ctx.status, message: e.message });
+  }
+};
+
+Public.get("/", Fan);
